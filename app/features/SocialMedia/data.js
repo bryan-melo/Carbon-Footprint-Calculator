a899c48c9e650983d59f94b41dacfc4c21e5d46d
NhaleBoy <104642921+NhaleBoy@users.noreply.github.com>
2024-11-03 21:54:30 -0800

    Added social media feature

---
+++ app/features/SocialMedia/data.js
@@ -0,0 +1,41 @@
+var threads = [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Gojo",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Benny",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "John",
+                date: Date.now(),
+                content: "Hey there to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Geto",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Benny",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "John",
+                date: Date.now(),
+                content: "Hey there to you too"
+            }
+        ]
+    }
+
+]
