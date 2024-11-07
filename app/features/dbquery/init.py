162d5f09216c2e3681a3e6be2199b6aa27dc2c26
RepiMinji <lancele195@gmail.com>
2024-11-06 18:18:16 -0800

    added comment table
    changed some stuff around on db

--- app/features/dbquery/init.py
+++ app/features/dbquery/init.py
@@ -5,8 +5,12 @@ cur = con.cursor()
 # SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY, username TEXT, password TEXT, emissions REAL)'''
 # cur.execute(SQL_CREATE_TABLE)
 
-# SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS post(post_id INTEGER PRIMARY KEY,  user_id INTEGER, post_title TEXT,  post_date DATE)'''
+# SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS post(post_id INTEGER PRIMARY KEY,  user_id INTEGER, post_title TEXT,  post_date DATE, post_text TEXT)'''
 # cur.execute(SQL_CREATE_TABLE)
+
+# SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS comment(comment_id INTEGER PRIMARY KEY, user_id INTEGER, post_id INTEGER, comment_text TEXT)'''
+# cur.execute(SQL_CREATE_TABLE)
+
 # con.commit()
 
 # cur.execute("""
