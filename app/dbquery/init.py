99b1775fccc6ee2b937d520194d6ba8227e38252
RepiMinji <lancele195@gmail.com>
2024-11-16 21:42:52 -0800

    comment/thread
    Changed some variables in thread frontend, created comment frontend (similar structure to thread), changed database tables

--- app/dbquery/init.py
+++ app/dbquery/init.py
@@ -2,16 +2,15 @@ import sqlite3
 con = sqlite3.connect("app/carbon_calc.db")
 cur = con.cursor()
 
-# SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY, username TEXT, password TEXT, emissions REAL)'''
-# cur.execute(SQL_CREATE_TABLE)
-
 # SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS post(post_id INTEGER PRIMARY KEY,  user_id INTEGER, post_title TEXT,  post_date DATE, post_text TEXT)'''
 # cur.execute(SQL_CREATE_TABLE)
 
 # SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS comment(comment_id INTEGER PRIMARY KEY, user_id INTEGER, post_id INTEGER, comment_text TEXT)'''
 # cur.execute(SQL_CREATE_TABLE)
 
-# con.commit()
+cur.execute("""ALTER TABLE comment ADD comment_date DATE""")
+
+con.commit()
 
 # cur.execute("""
 #                 INSERT INTO user VALUES
@@ -20,6 +19,9 @@ cur = con.cursor()
 # con.commit()
 # con.close()
 
-res = cur.execute("SELECT * FROM user")
-p = res.fetchall()
-print(p)
+# res = cur.execute("SELECT * FROM user")
+# p = res.fetchall()
+# print(p)
+
+# cur.execute("""DROP TABLE user""")
+# con.commit()
